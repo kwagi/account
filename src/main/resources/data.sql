09fe79ad8b042c3132bff80d44c5856d62df3cab
kwagi <heisenberg123457@gmail.com>
2022-06-24 08:07:41 +0900

    new branch test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ insert into account_user(id, name, created_at, updated_at)
 values (1, 'kanye', now(), now())
 ;
 insert into account_user(id, name, created_at, updated_at)
-values (2, 'Tory', now(), now())
+values (2, 'Tory Lanze', now(), now())
 ;
