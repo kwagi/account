bda0c1e6a811508dcfc09d24dfc16713fd18e81b
kwagi <heisenberg123457@gmail.com>
2022-06-24 08:16:49 +0900

    random accountNum

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,9 @@ values (1, 'kanye', now(), now())
 insert into account_user(id, name, created_at, updated_at)
 values (2, 'Tory', now(), now())
 ;
+insert into account_user(id, name, created_at, updated_at)
+values (3, 'Polo', now(), now())
+;
+insert into account_user(id, name, created_at, updated_at)
+values (4, 'Fivio', now(), now())
+;
