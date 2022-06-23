79926916c2ce68bb1ff4faab5e0c19f181672295
kwagi <heisenberg123457@gmail.com>
2022-06-23 16:37:23 +0900

    005 end

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 insert into account_user(id, name, created_at, updated_at)
 values (1, 'kanye', now(), now())
 ;
+insert into account_user(id, name, created_at, updated_at)
+values (2, 'Tory', now(), now())
+;
+insert into account_user(id, name, created_at, updated_at)
+values (1, 'Carti', now(), now())
+;
--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -7,6 +7,7 @@ import com.example.account.repository.AccountRepository;
 import com.example.account.repository.AccountUserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -17,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
@@ -49,9 +52,12 @@ class AccountServiceTest {
                         .accountUser(user)
                         .accountNumber("1000000013")
                         .build());
+
+        ArgumentCaptor<Account> captor = ArgumentCaptor.forClass(Account.class);
         //when
         AccountDto accountDto = accountService.createAccount(100L, 1000L);
         //then
+        verify(accountRepository,times(1)).save(captor.capture());
         assertEquals(12L, accountDto.getUserId());
         assertEquals("1000000013", accountDto.getAccountNumber());
     }
