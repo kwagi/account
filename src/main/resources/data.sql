957e6774808c7e90b80c39b6eaa78aa0dc7839ac
kwagi <heisenberg123457@gmail.com>
2022-06-24 08:11:31 +0900

    create account complete

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,8 @@ insert into account_user(id, name, created_at, updated_at)
 values (1, 'kanye', now(), now())
 ;
 insert into account_user(id, name, created_at, updated_at)
-values (2, 'Tory Lanze', now(), now())
+values (2, 'carti', now(), now())
+;
+insert into account_user(id, name, created_at, updated_at)
+values (3, 'Tjay', now(), now())
 ;
