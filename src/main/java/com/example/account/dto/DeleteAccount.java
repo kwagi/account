ff136f6e46a5a90a6a52a42a6c3867e55f57031a
kwagi <heisenberg123457@gmail.com>
2022-06-24 13:59:23 +0900

    DeleteAccount001

--- src/main/java/com/example/account/dto/DeleteAccount.java
+++ src/main/java/com/example/account/dto/DeleteAccount.java
@@ -3,10 +3,12 @@ package com.example.account.dto;
 import lombok.*;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
-public class CreateAccount {
+public class DeleteAccount {
     @Getter
     @Setter
     @AllArgsConstructor
@@ -14,9 +16,9 @@ public class CreateAccount {
         @NotNull
         @Min(1)
         private Long userId;
-        @NotNull
-        @Min(0)//0원이상
-        private Long initialBalance;
+        @NotBlank
+        @Size(min = 10, max = 10)
+        private String accountNumber;
     }
 
     @Getter
@@ -27,13 +29,13 @@ public class CreateAccount {
     public static class Response {
         private Long userId;
         private String accountNumber;
-        private LocalDateTime registeredAt;
+        private LocalDateTime unRegisteredAt;
 
         public static Response from(AccountDto accountDto) {
             return Response.builder()
                     .userId(accountDto.getUserId())
                     .accountNumber(accountDto.getAccountNumber())
-                    .registeredAt(accountDto.getRegisteredAt())
+                    .unRegisteredAt(accountDto.getUnRegisteredAt())
                     .build();
         }
     }
--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -6,8 +6,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자가 없음"),
-    USER_MAX_DETECTED("계좌 최대갯수 도달");
+    USER_NOT_FOUND("사용자가 없습니다."),
+    ACCOUNT_NOT_FOUND("계좌가 없습니다."),
+    USER_NOT_MATCHED("사용자와 계좌정보가 맞지않습니다."),
+    ACCOUNT_ALREADY_UNREGISTERED("이미 해지되었습니다."),
+    BALANCE_NOT_EMPTY("잔액이 존재하여 해지할수없습니다."),
+    USER_MAX_DETECTED("사용자당 가질수있는 최대계좌갯수는 10개입니다.");
 
     private final String description;
 }
